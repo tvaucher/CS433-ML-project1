f427eb42b467fd2cfae7d5abe8e3fbdca6c16d14
Andrej Janchevski <janchevski.andrej@gmail.com>
2019-10-25 10:38:03 +0200

    Move train-test-split function to the cross validation script

--- cross_validation.py
+++ cross_validation.py
@@ -1,5 +1,38 @@
 import numpy as np
 
+
+def train_test_split_data(y, x, ratio, seed):
+    """
+    Helper function that splits data samples randomly into train and test sets by a given ratio
+
+    :param y: vector of target values, numpy array with dimensions (N, 1)
+    :param x: data matrix, numpy ndarray with shape with shape (N, D),
+              where N is the number of samples and D is the number of features
+    :param ratio: fraction of data samples that will be selected for trainings, float value between 0 and 1
+    :param seed: seeding value for the random generator, integer
+
+    :returns: tuple (x_train, y_train, x_test, y_test),
+              contains samples and targets in train set, followed by samples and targets in test set
+    """
+
+    # Set the seed
+    np.random.seed(seed)
+
+    # Use the ratio to find the number of samples in the train set as an integer by rounding if necessary
+    n = x.shape[0]
+    num_train_samples = int(ratio * n)
+
+    # Shuffle and split the sample indices into train and test parts
+    indices = np.random.permutation(n)
+    train_indices = indices[:num_train_samples]
+    test_indices = indices[num_train_samples:]
+
+    # Generate the output sets
+    x_train, y_train = x[train_indices], y[train_indices]
+    x_test, y_test = x[test_indices], y[test_indices]
+    return x_train, y_train, x_test, y_test
+
+
 def k_fold_cross_split_data(y, x, k, seed):
     """
     Helper function that splits data samples randomly into k folds, to be used afterwards for cross-validation
